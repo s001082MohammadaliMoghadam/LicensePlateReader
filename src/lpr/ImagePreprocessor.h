b1d682cff8061ad843562a752ee773abc5ba45d0
s001082MohammadaliMoghadam <moghadamjoz@gmail.com>
2026-09-15 12:29:57 +0330

    feat: add morphology closing and plate candidate detection

--- src/lpr/ImagePreprocessor.h
+++ src/lpr/ImagePreprocessor.h
@@ -5,5 +5,6 @@ class ImagePreprocessor {
 public:
     cv::Mat grayscale(const cv::Mat& image) const;
     cv::Mat edges(const cv::Mat& image, const DetectionConfig& config) const;
+    cv::Mat closeEdges(const cv::Mat& edgeImage, const DetectionConfig& config) const;
 };
 }
